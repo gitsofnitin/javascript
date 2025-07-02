9a4ecd138fd9769b235708fe0ff811745502c5a5
nitin kumar <143083609+gitsofnitin@users.noreply.github.com>
2025-07-02 00:28:01 +0000

    container is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accId =  144553
+let  accEmail = "nitin@google.com"
+var  accPass = "12345"
+accCity = " jaipur"
+let accountState;
+accEmail = "name.google"
+accPass  = "232345"
+accCity = " delhi"
+
+
+console.log([accId,accEmail,accPass,accCity,accountState])
