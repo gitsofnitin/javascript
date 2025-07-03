37767cfe20ba59db24c51dc2e0af486c78f889e1
nitin kumar <143083609+gitsofnitin@users.noreply.github.com>
2025-07-03 13:28:13 +0000

    operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,67 @@
+/*
+let score  = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+
+
+let isLoggedIn = "hitesh"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+*/
+
+
+// ********* OPERATIONS **************
+
+
+
+/*
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+*/
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);  // 2^3
+console.log(2/2);
+console.log(2%2);
+
+*/
+
+let str1 = "hello"
+let str2 = " nitin"
+
+str3= str1+ str2
+console.log(str3);
+
+console.log("1" +  2);
+console.log(1+"2");
+console.log("1" +  2 +  2);
+console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
