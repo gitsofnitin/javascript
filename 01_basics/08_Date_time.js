eb244495e8c44a188363905323aab1db73078983
nitin kumar <143083609+gitsofnitin@users.noreply.github.com>
2025-07-06 01:49:09 +0000

    date and time

--- 01_basics/08_Date_time.js
+++ 01_basics/08_Date_time.js
@@ -1,5 +1,38 @@
 // DATES
 
+/*
 let myDate = new Date()
 console.log(myDate)
 console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
+
+
+let myCreatedDate = new Date (2023,0,23)
+ console.log(myCreatedDate.toDateString())
+ let myCreatedDate1 = new Date ("2023-01-23")
+console.log(myCreatedDate1.toLocaleString())
+let myCreatedDate11 = new Date ("23-01-2023")
+console.log(myCreatedDate11.toLocaleString())
+
+*/
+//===================== TIME =========================================================
+
+
+/*
+let myTimeStamp = Date.now()
+ console.log(myTimeStamp);
+ let myCreatedDate1 = new Date ("2023-01-23")
+console.log(myCreatedDate1.getTime());
+
+ console.log(Date.now()/1000);
+ console.log(Math.floor(Date.now()/1000));
+*/
+
+let newDate =  new Date()
+console.log(newDate)
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay()); // on sundey --> 0, on monday --> 1
+ 
+ 
