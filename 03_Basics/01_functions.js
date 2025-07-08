1eebd04bf3d126666411ce7771957bc6e92378ef
nitin kumar <143083609+gitsofnitin@users.noreply.github.com>
2025-07-08 08:13:09 +0000

    functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,72 @@
+function sayMyName () {
+    console.log("H");
+    console.log("i");
+    console.log("J");
+    console.log("L");
+}
+ 
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+
+//=========LECTURE 20 =================
+
+
+
+function calculateCartPrice( ...num1){  // this is called rest operator:that allows you to collect an indefinite number of arguments into an array
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
